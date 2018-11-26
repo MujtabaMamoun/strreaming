52821353bfb4fd28caa38e6c6bba6a9720f759d9
MujtabaMamoun <44872348+MujtabaMamoun@users.noreply.github.com>
2018-11-26 06:17:15 -0800

    Add files via upload

---
+++ Break.js
@@ -0,0 +1,62 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+
+client.on('ready', () => {
+  console.log('')
+  console.log('')
+  console.log('╔[═════════════════════════════════════════════════════════════════]╗')
+  console.log(`[Start] ${new Date()}`);
+  console.log('╚[═════════════════════════════════════════════════════════════════]╝')
+  console.log('')
+  console.log('╔[════════════════════════════════════]╗');
+  console.log(`Logged in as * [ " ${client.user.username} " ]`);
+  console.log('')
+  console.log('Informations :')
+  console.log('')
+  console.log(`servers! [ " ${client.guilds.size} " ]`);
+  console.log(`Users! [ " ${client.users.size} " ]`);
+  console.log(`channels! [ " ${client.channels.size} " ]`);
+  console.log('╚[════════════════════════════════════]╝')
+  console.log('')
+  console.log('╔[════════════]╗')
+  console.log(' Bot Is Online')
+  console.log('╚[════════════]╝')
+  console.log('')
+  console.log('')
+});
+
+
+
+
+const adminprefix = "$";
+const devs = ['474573718967025665'];
+client.on('message', message => {
+    var argresult = message.content.split(` `).slice(1).join(' ');
+    if (!devs.includes(message.author.id)) return;
+
+    if (message.content.startsWith(adminprefix + 'setstreem')) {
+        client.user.setGame(argresult, "https://www.twitch.tv/idk");
+        message.channel.sendMessage(`**تم تغيير تويتش البوت إلى  ${argresult}**`)
+    }
+
+});
+
+
+client.on('message', function (message) {
+    let args = message.content.split(" ").slice(1).join(" ");
+    if (message.content.startsWith(adminprefix + "setWatch")) {
+        if (message.author.id !== '474573718967025665') return;
+        message.channel.send("**- :white_check_mark: Done!,**");
+    }
+});
+client.on('message', function (message) {
+    let args = message.content.split(" ").slice(1).join(" ");
+    if (message.content.startsWith(adminprefix + "setListen")) {
+        if (message.author.id !== '474573718967025665') return;
+        client.user.setActivity(args, { type: 'LISTENING' });
+        message.channel.send("**- :white_check_mark: Done!,**");
+    }
+});
+
+client.login(process.env.BOT_TOKEN);
